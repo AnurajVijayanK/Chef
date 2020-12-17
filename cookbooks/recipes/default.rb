722f87859293d42553b9fa5aa1be353c513f3863
anurajvijayank <anurajvijayan.pba@gmail.com>
2020-12-17 17:46:56 +0530

    myrepo updated for chef configuration 17.12.2020

---
+++ cookbooks/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cookbooks
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
---
+++ roles/user.rb
@@ -0,0 +1,3 @@
+name 'user'
+description 'Base Role for all Servers'
+run_list 'recipe[myusers]'
